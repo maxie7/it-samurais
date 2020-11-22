15ad9eeb8628c5233d29f71d295815620960f388
maxie7 <maxie7@protonmail.com>
2020-11-22 13:38:13 +0100

    Updates

---
+++ precache-manifest.c0beb53b52003603143b200895238c26.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1be7c7bae5f2ad4dccd4dae64208d85",
+    "url": "/it-samurais/index.html"
+  },
+  {
+    "revision": "59962b537cec6922c5e1",
+    "url": "/it-samurais/static/css/3.c1ace39e.chunk.css"
+  },
+  {
+    "revision": "04eec012cf70e9215066",
+    "url": "/it-samurais/static/css/4.26e6ea2e.chunk.css"
+  },
+  {
+    "revision": "1cd84a4974dd60adf26a",
+    "url": "/it-samurais/static/css/main.3bde656b.chunk.css"
+  },
+  {
+    "revision": "ee5951b42d7f679f6c6b",
+    "url": "/it-samurais/static/js/2.0bde96bd.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/it-samurais/static/js/2.0bde96bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59962b537cec6922c5e1",
+    "url": "/it-samurais/static/js/3.46d0f225.chunk.js"
+  },
+  {
+    "revision": "04eec012cf70e9215066",
+    "url": "/it-samurais/static/js/4.c9a8c6f0.chunk.js"
+  },
+  {
+    "revision": "1cd84a4974dd60adf26a",
+    "url": "/it-samurais/static/js/main.3975e39d.chunk.js"
+  },
+  {
+    "revision": "963a3ff7bb50dbbfa3f7",
+    "url": "/it-samurais/static/js/runtime-main.90d59be0.js"
+  },
+  {
+    "revision": "7d5d465b0c02e9451581dad83be7bdc3",
+    "url": "/it-samurais/static/media/logo.7d5d465b.svg"
+  },
+  {
+    "revision": "6304b5e597602b87211f2131a27bd84f",
+    "url": "/it-samurais/static/media/spinner.6304b5e5.svg"
+  }
+]);
