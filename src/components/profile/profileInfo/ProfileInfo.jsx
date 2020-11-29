dbcab0c173ae2b0d35b40410150bcecb1dd06b01
maxie7 <maxie7@protonmail.com>
2020-11-29 12:10:37 +0100

    Fix shouldComponentUpdate; Add new feature >> upload profile photo (change api && profile reducer)

--- src/components/profile/profileInfo/ProfileInfo.jsx
+++ src/components/profile/profileInfo/ProfileInfo.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import classes from './ProfileInfo.module.css';
 import Preloader from "../../common/preloader/preloader";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
+import userPhoto from "../../../assets/img/default_avatar.jpeg";
 
-const ProfileInfo = ({profile, status, updateStatus}) => {
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
   if (!profile) {
     return <Preloader />
+  }
+
+  const onMainPhotoSelected = (e) => {
+    if (e.target.files.length) {
+      savePhoto(e.target.files[0]);
+    }
   }
 
   return (
     <div>
       <div className={classes.descriptionBlock}>
-        <img alt='' src={profile.photos.large} />
+        <img alt='' src={profile.photos.large || userPhoto} className={classes.mainPhoto} />
+        { isOwner && <input type={"file"} onChange={onMainPhotoSelected} /> }
         <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
       </div>
     </div>
